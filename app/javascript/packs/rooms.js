be1b4470f3cb7f1d91cfe818b16f6f56457ce890
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-05-29 11:13:38 +0900

    use 'semi': ['error', 'always']

--- app/javascript/packs/rooms.js
+++ app/javascript/packs/rooms.js
@@ -1,26 +1,26 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import {
   Link,
   Table,
   TableColumn
-} from 'element-ui'
-import RoomsTable from '../RoomsTable.vue'
+} from 'element-ui';
+import RoomsTable from '../RoomsTable.vue';
 
-Vue.use(Link)
-Vue.use(Table)
-Vue.use(TableColumn)
+Vue.use(Link);
+Vue.use(Table);
+Vue.use(TableColumn);
 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.querySelector('.js-rooms-table')
+  const el = document.querySelector('.js-rooms-table');
 
-  const { rooms } = el.dataset
+  const { rooms } = el.dataset;
 
   const props = {
     rooms: rooms ? JSON.parse(rooms) : []
-  }
+  };
 
   new Vue({
     el: el,
     render: h => h(RoomsTable, { props })
-  }).$mount()
-})
+  }).$mount();
+});
