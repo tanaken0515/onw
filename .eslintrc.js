784d58a5516df38a36ac13ba9b8c2a1845928916
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-05-29 11:16:36 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        'semi': ['error', 'always']
-    }
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/essential'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 11,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue'
+  ],
+  'rules': {
+    'semi': ['error', 'always']
+  }
 };
