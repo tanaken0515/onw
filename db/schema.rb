5b137d23ff9f903ff73e14fc6ac17bae12f3411e
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-06-03 10:21:35 +0900

    change url_token null false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_004115) do
+ActiveRecord::Schema.define(version: 2020_06_03_011641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_004115) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "url_token", limit: 30
+    t.string "url_token", limit: 30, null: false
   end
 
 end
