f01dbf07509c14a5211df754e204e4036f6e77c7
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-06-03 10:13:22 +0900

    set rooms url_token before validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,11 @@
 class Room < ApplicationRecord
+  include HasUrlToken
+
+  before_validation :set_url_token
+
+  private
+
+  def set_url_token
+    self.url_token ||= self.class.generate_token
+  end
 end
